3f0b2467c40e75a8ded0ad02dfabf674249bb611
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-06-21 13:53:55 +0530

    newTask added to todoapp

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,22 +1,21 @@
 import "./Home.css"
 import add from "./plus.png"
 import ToDoCard from "../../components/todoCard/todoCard"
+import { useState } from "react"
 function Home() {
-    const todolist = [
-        "Go to gym",
-        "buy grocery",
+  
+    const [todoList, setTodoList] = useState([
         "finish project",
-        "household",
-        "pay bill",
-        "watch movie"
-    ]
+         "pay bill",
+    ])
+    const [newTask, setNewTask] = useState("")
 
   return (
     <div>
         <h1 className="app-title">To-Do App📝</h1>
         <div className="todolist-container">
            {
-            todolist.map((todoItem, i)=>{
+            todoList.map((todoItem, i)=>{
                 return(
                     <ToDoCard key={i} todoItem={todoItem}/>
                 )
@@ -24,8 +23,22 @@ function Home() {
            }
         </div>
         <div className="add-item-container">
-            <input type="text" placeholder="Add your Task" className="add-input"/>
-            <img src={add} alt="add-btn" className="add-icon"/>
+            <input
+                type="text"
+                placeholder="Add your Task"
+                className="add-input"
+                value={newTask}
+                onChange={(e)=>setNewTask(e.target.value)}
+            />
+            <img 
+                src={add} 
+                alt="add-btn" 
+                className="add-icon"
+                onClick={()=>{
+                    setTodoList([...todoList, newTask])
+                    setNewTask("")
+                }}
+            />
         </div>
     </div>
   )
